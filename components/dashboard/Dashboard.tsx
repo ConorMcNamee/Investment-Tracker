67d6d9e8190c8352bcd7fce42c5cb111b4a2d591
Conor <cmcnamee4@gmail.com>
2021-11-30 01:18:12 +0000

    created listview for dashboard

--- components/dashboard/Dashboard.tsx
+++ components/dashboard/Dashboard.tsx
@@ -5,6 +5,7 @@ import { View, Text } from "react-native";
 import styles from "../../styles/mainStyles";
 
 interface Investment {
+    id: number,
     investmentTitle: string,
     amount: number,
     monthlyAmount: number
@@ -13,11 +14,24 @@ interface Investment {
 
 const Dashboard = () => {
 
-    const [investments, addInvestments] = useState<Investment[]>();
+    const [investments, addInvestments] = useState<Investment[]>(
+        [
+            {id: 0, investmentTitle: 'bitcoin', amount: 100, monthlyAmount: 100},
+            {id: 1, investmentTitle: 'Luna ', amount: 100, monthlyAmount: 100}
+        ]
+    );
+
+
 
     return (
         <View>
-            <Text style={styles.text}>Hello World</Text>
+            {
+                investments.map((investment) => {
+                    return (
+                        <Text style={styles.text} key={investment.id}>{investment.investmentTitle}</Text>
+                    )
+                })
+            }
         </View>
     );
 }
